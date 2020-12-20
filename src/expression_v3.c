f1c3e57d9d196ac7f9804d7979d8694f2b551367
Z4cki <legocrack@freenet.de>
2020-12-20 16:43:24 +0100

    made it work with multiple digit numbers

--- src/expression_v3.c
+++ src/expression_v3.c
@@ -2,19 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void appendChar(char * s, char c)
-{
-    size_t len = strlen(s);
-    if (len < 254)
-    {
-        s[len] = c;
-        s[len + 1] = '\0';
-    }
-    else 
-    {
-        printf("Not enough space\n");
-    }
-}
+void appendchar(char * s, char c);
+void appendfloat(float * f, char c);
 
 int main()
 {
@@ -23,63 +12,84 @@ int main()
     char c, current_op;
 
     // the build number
-    char current_num[256] = "";
+    char input[256] = "";
 
-    // indicates if a number is currently being build
-    int num_flag = 0;
-
-    float a;
+    float result, f = 0.0;
 
     printf("Enter expression: \n");
 
-    while ((c = getchar()) != '\n' || num_flag == (c == '\n'))
+    // read the input
+    while ((c = getchar()) != '\n')
     {
-        // subtract '0' gives us the numeric value         
+        // ignore spaces
+        if (c == 32) continue;
+        appendchar(input, c);
+    }
+
+    size_t len = strlen(input);
+    for (int i = 0; i <= len; i++)
+    {
+        if (i < len) c = input[i];
+        else c = '+';
+        // subtract '0' gives us the numeric value
         if (c - '0' < 0)
         {
-            num_flag = 1;
+            switch (current_op) 
+            {
+            case '+':
+                result += f;
+                break;
+            case '-':
+                result -= f;
+                break;
+            case '*':
+                result *= f;
+                break;
+            case '/':
+                result /= f;
+                break;
+            default:
+                result = f;
+            }
+            f = 0.0;
         }
         // if we have got an actual number
         else
         {
-            appendChar(current_num, c - '0');
-        }
-        
-        if(num_flag)
-        {
-            float f = (float) *current_num;
-            switch (current_op) 
-            {
-            case '+':
-                a += f;
-                break;
-            case '-':
-                a -= f;
-                break;
-            case '*':
-                a *= f;
-                break;
-            case '/':
-                a /= f;
-                break;
-            default:
-                a = f;
-            }
-            num_flag = 0;
-            current_num[0] = '\0';
+            appendfloat(&f, c - '0');
         }
 
         if (c - '0' < 0)
         {
             current_op = c;
         }
-
     }
-    
 
-    printf("Value of expression: %.1f\n\n", a);
+    printf("Value of expression: %.1f\n\n", result);
 
     // pause the console, must include stdlib.h
     system("pause");                       
     return 0;
+}
+
+void appendchar(char * s, char c)
+{
+    size_t len = strlen(s);
+    if (len < 254)
+    {
+        s[len] = c;
+        s[len+1] = '\0';
+    }
+}
+
+void appendfloat(float * f, char c)
+{
+    if (*f == 0.0)
+    {
+        *f = c;
+    }
+    else
+    {
+        *f = *f * 10 + c;
+    }
 }
