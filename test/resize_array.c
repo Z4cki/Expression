7cbdb2f016778bf4e4d9147b2b339ffaae50db2b
Z4cki <legocrack@freenet.de>
2020-12-20 14:09:48 +0100

    test

--- test/resize_array.c
+++ test/resize_array.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    
+    char * s = {"name"};
+    char c = 'n';
+    char temp[256];
+    printf("%d\n", sizeof(temp));
+    system("pause");
+    return 0;
 }
