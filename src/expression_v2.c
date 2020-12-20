598210413babc855524a270e897b90751e4dcf31
Z4cki <legocrack@freenet.de>
2020-12-20 10:35:08 +0100

    inital code commit, added json to gitignore

---
+++ src/expression_v2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // c stores the current character, current_op the last operand detected
+    char c, current_op;
+    float a;
+
+    printf("Enter expression: \n");
+
+
+    while ((c = getchar()) != '\n')
+    {
+        // space, we ignore it
+        if (c == 32) continue;
+        // subtract '0' gives us the numeric value         
+        if (c - '0' < 0) 
+        {
+            current_op = c;
+            continue;
+        }
+
+        // if we have got a actual number we take the last operand and perform the magic
+        c = c - '0';
+        switch (current_op) {                                                        
+        case '+':                                                           
+            a += (float) c;
+            break;                                                          
+        case '-':                                                           
+            a -= (float) c;                                                 
+            break;                                                          
+        case '*':                                                           
+            a *= (float) c;                                                 
+            break;                                                          
+        case '/':                                                           
+            a /= (float) c;                                                 
+            break;  
+        default:
+            a = (float) c;                        
+        }                                                         
+    }
+
+    printf("Value of expression: %.1f\n\n", a);
+
+    // pause the console, must include stdlib.h
+    system("pause");                       
+    return 0;
+}
